6d8abb9e71c77f1b7d592076e92d22da11c9be36
prakhe <81826566+prakhe@users.noreply.github.com>
2021-05-10 18:18:24 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Input
+{
+    public static void main(String args[]) throws IOException
+    {
+        Scanner sc=new Scanner(System.in);
+        FileWriter fileWriter=new FileWriter("IOS.csv");
+        PrintWriter printWriter=new PrintWriter(fileWriter);
+        System.out.println("Enter your name and age=" );
+        String name=sc.next();
+        String age=sc.next();
+        String sentence="\n"+name+","+age;
+        System.out.println(sentence);
+        printWriter.append(sentence);
+    }
+
+}
