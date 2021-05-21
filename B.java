511c2b7b84c0bb2b5aa482a8c2cd2d7a30fcc6bc
prakhe <81826566+prakhe@users.noreply.github.com>
2021-05-21 14:42:45 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class B
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        for(x=1;x<=5;x++)
+        {
+            for(y=1;y<=x;y++)
+            {
+                if (y%2==0)
+                {
+                    System.out.print("{");
+                }
+                else
+                {
+                    System.out.print("[");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
