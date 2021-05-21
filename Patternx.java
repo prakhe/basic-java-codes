a098c2e6088ac982e42be97d110763cff5072dc8
prakhe <81826566+prakhe@users.noreply.github.com>
2021-05-21 19:48:37 +0530

    Add files via upload

---
+++ Patternx.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Patternx
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y;
+        for(x=1;x<=6;x++)
+        {
+            for(y=1;y<=x;y++)
+            {
+                if (y%2==0)
+                {
+                    System.out.print(">");
+                }
+                else
+                {
+                    System.out.print("<");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
