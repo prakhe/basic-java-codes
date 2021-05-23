db28bf4eabbc208411d4da2b227f30a366d06307
prakhe <81826566+prakhe@users.noreply.github.com>
2021-05-23 12:14:11 +0530

    Add files via upload

---
+++ Best.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Best extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("I AM THE BEST",100,100);
+    }
+}
